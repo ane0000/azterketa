2a7ed95c74ebf0a2eb4f323ab37c101084bf4c49
Ane <a_izaguirrelor@fpzornotza.com>
2024-11-21 10:28:16 +0100

    commit2

--- ENDEazterketa/src/principal/AneIzaguirre.java
+++ ENDEazterketa/src/principal/AneIzaguirre.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 public class AneIzaguirre{
 	private static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {   
-        int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
         
         Operaciones operaciones = new Operaciones();
        
-        System.out.print ("Operando 1: ");
-        operandos [0] = teclado.nextInt();
-        System.out.print ("Operando 2: ");
-        operandos [1] = teclado.nextInt();
+        PedirNumero(operandos);
            
-        System.out.print ("Operaciones [+, -, *, /, %]: ");
-        operacion = teclado.next();
-        while (!((operacion.equalsIgnoreCase("+")) || (operacion.equalsIgnoreCase("-")) ||
+        operacion =PedirOperecion(operacion);
+        
+        RealizarOperacion();
+        RealizarOperacion(operacion, operandos, operaciones);    		 
+        	
+    }
+	private static void RealizarOperacion(String operacion, int[] operandos, Operaciones operaciones) {
+		int resultado;
+		while (!((operacion.equalsIgnoreCase("+")) || (operacion.equalsIgnoreCase("-")) ||
                     (operacion.equalsIgnoreCase("*")) || (operacion.equalsIgnoreCase("/")) ||
                     (operacion.equalsIgnoreCase("%"))
                     )) 
@@ -44,7 +46,23 @@ public class AneIzaguirre{
                     System.out.println ("Resultado: " + resultado);
                 } else {
                     System.out.println ("Operaci�n no v�lida");
-                }    		 
-        	
+                }
+	}
+    public static void PedirNumero(int[] operandos) {
+    	System.out.print ("Operando 1: ");
+        operandos [0] = teclado.nextInt();
+        System.out.print ("Operando 2: ");
+        operandos [1] = teclado.nextInt();
+    }
+
+    public static String PedirOperecion(String operacion) {
+    	System.out.print ("Operaciones [+, -, *, /, %]: ");
+        operacion = teclado.next();
+        return operacion;
+    	
+    }
+
+    public static void RealizarOperacion() {
+    	
     }
 }
